fcc9c0e2a891610b544e1e03e05340bacc37923e
Rafa Hinojosa <rafael.zeo@gmail.com>
2021-07-16 13:22:46 -0500

    Conversations Set Up. Conversation Layout, Drawables of sent and received messages and Translate icon.

---
+++ app/src/main/java/com/example/duce/ConversationActivity.java
@@ -0,0 +1,14 @@
+package com.example.duce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConversationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conversation_activity);
+    }
+}
