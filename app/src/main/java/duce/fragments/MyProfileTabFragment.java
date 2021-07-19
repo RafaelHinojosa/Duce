2889dc9a56a711932ba58a08aa3d0df602f29443
Rafa Hinojosa <rafael.zeo@gmail.com>
2021-07-19 18:23:43 -0500

    Log Out implemented

--- app/src/main/java/duce/fragments/MyProfileTabFragment.java
+++ app/src/main/java/duce/fragments/MyProfileTabFragment.java
@@ -1,5 +1,6 @@
 package duce.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.duce.R;
+import com.duce.databinding.MyProfileTabFragmentBinding;
+import com.parse.ParseUser;
+
+import duce.StartActivity;
 
 public class MyProfileTabFragment extends Fragment {
+
+    private static final String TAG = "MyProfileTabFragment";
+
+    private Button mBtnLogOut;
 
     public MyProfileTabFragment() {
         // Required empty public constructor
@@ -22,14 +32,25 @@ public class MyProfileTabFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mBtnLogOut = view.findViewById(R.id.btnLogOut);
+
+        mBtnLogOut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Intent toStartActivity = new Intent(getActivity(), StartActivity.class);
+                startActivity(toStartActivity);
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.my_profile_tab_fragment, container, false);
     }
 }
